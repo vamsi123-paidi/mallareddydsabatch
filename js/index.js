cf0f0ae6c87300401ba79ce45ddb89b20f3b9006
vamsi123-paidi <vamsipaidi251002@gmail.com>
2026-09-02 21:56:36 +0530

    9/2

---
+++ js/index.js
@@ -0,0 +1,179 @@
+// console.log("hello world")
+
+// var a = 10;
+// var a = 20;
+// a=30
+// console.log(a)
+
+// let a =10;
+// // let a = 20
+// a= 20
+// console.log(a)
+
+// const a = 10;
+// // const a = 20
+// a = 20
+// console.log(a)
+
+
+// console.log(a)
+// let a = 10
+
+
+// {
+//     const a = 10;
+// }
+//     console.log(a)
+
+
+// in javascript the collection of items of same or different datatypes is called array
+
+// const arr = [1,"adam",true,null]
+
+// console.log(arr[7])
+// arr.push(6)
+// arr.pop()
+// arr.unshift(0)
+// arr.shift()
+// const slicedValue = arr.slice(2,4)
+// console.log(slicedValue)
+
+// const obj = {
+//     name:"adam",
+//     age:35,
+//     city:"hydrebad"
+// }
+// console.log(obj.name)
+
+
+
+// console.log(10+20)
+
+// console.log(true && true)
+// let a = 10
+// console.log(--a)
+
+// if(false){
+// console.log("hello")
+// }else{
+//     console.log("check the error")
+// }
+
+// const age = -233;
+// if(age>=5 && age<=12){
+//     console.log("ticket price 20$")
+// }else if(age>12 && age<=40){
+//     console.log("ticket price 40$")
+// }else if(age>40 && age<=120){
+//     console.log("ticket price 10$")
+// }else{
+//     console.log("invalid age")
+// }
+
+// while(false){
+//     console.log("hello")
+// }
+
+// do{
+//     console.log("hello")
+// }while(false)
+
+
+// let i = 1;
+// while(i>=5){
+//     console.log(i)
+//     i++
+// }
+
+
+// let i =1
+// do{
+//     console.log(i)
+//     i++
+// }while(i>=5)
+// for i in range(1,10):
+//     print(i)
+// for(let i =1;i<=10;i++){
+//     console.log(i)
+// }
+// const arr = [1,2,3,4,5]
+// for(let i = 0;i<arr.length;i++){
+//     if(arr[i]%2!==0){
+//     console.log(arr[i])
+// }}
+
+// const arr = [1,2,3,5,6]
+
+// for(value of arr){
+//     console.log(value)
+// }
+
+// const str = "adam"
+// for (text of str){
+//     console.log(text)
+// }
+
+// const arr = [1,2,3,4,5]
+// for (index in arr){
+//     console.log(index)
+// }
+
+// const obj = {
+//     name:"adam",
+//     age:100
+// }
+// for (key in obj){
+//     console.log(obj[key])
+// }
+
+// named function
+
+// function greet(){
+//     //boc
+// }
+// greet()
+
+
+// function add(){
+//     const a = 10
+//     const b = 20
+//     const c = a+b
+//     console.log(c)
+// }
+// add()
+
+// function add(a,b){
+//     return a+b
+// }
+// console.log(add(1,2))
+// console.log(add(3,4))
+
+// anynomous function and fun expression
+// const add = function (){
+//     //boc
+// }
+// add()
+
+// const add = function (){
+//     console.log("hello ")
+// }
+// add()
+
+//arrow function 
+
+// const add = ()=>{
+//    console.log("hello") 
+// }
+// add()
+
+const add =(a,a,b,b,c)=>{
+    return a+a+b+b+c
+}
+console.log(add(1,2,3,6,5))
+
+
+
+
+
+
+
